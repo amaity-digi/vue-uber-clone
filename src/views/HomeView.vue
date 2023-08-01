9e8b195fac4af343bcb8052498a15b77475fdabf
Ashis Maity <amaity@digisprint.com>
2023-08-01 18:35:04 +0530

    initial commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="HOME" class="flex flex-col h-screen">
+    <div class="m-3 mb-auto">
+
+      <div class="grid grid-cols-2 gap-3 my-3">
+        <ServiceSelectLarge text="Ride" imgageWidth="100" image="ride" />
+        <ServiceSelectLarge text="Food" imgageWidth="100" image="food" promo="true" />
+      </div>
+
+      <div class="grid grid-cols-4 gap-3 my-3">
+        <ServiceSelectSmall text="Travel" image="travel" />
+        <ServiceSelectSmall text="2 Wheels" image="2wheels" />
+        <ServiceSelectSmall text="Transit" image="transit" />
+        <ServiceSelectSmall text="Reserve" image="reserve" />
+      </div>
+      <div>
+        <mdiMagnify size="40" />
+      </div>
+
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import ServiceSelectSmall from '@/components/ServiceSelectSmall.vue';
+import ServiceSelectLarge from '../components/ServiceSelectLarge.vue'
 </script>
